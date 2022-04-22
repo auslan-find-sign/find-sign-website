788cc3ad8b0e19fd479b0bee98407a6b3d7258f3
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-04-23 00:10:50 +1000

    move tests to a test folder route

---
+++ src/routes/tests/loading-demo.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Spinner from '$lib/Spinner.svelte'
+
+  let active = true
+</script>
+
+<Spinner bind:active/>
+
+<button on:click={() => active = true}>load</button>
+<button on:click={() => active = false}>finish</button>
