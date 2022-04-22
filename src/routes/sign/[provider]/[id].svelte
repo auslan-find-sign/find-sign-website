0a209d29172c3b2ae2e6346603908d509c85b45e
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-04-23 00:12:11 +1000

    changed /sign/[provider]/[id] route to be able to run entirely client side

--- src/routes/sign/[provider]/[id].svelte
+++ src/routes/sign/[provider]/[id].svelte
@@ -1,10 +1,35 @@
+<script lang="ts" context="module">
+  import siteConfig from '$lib/site-config.json'
+  import { open, getResultByPath } from '$lib/search/search-index'
+
+  export async function load ({ params }) {
+    const library = await open(siteConfig.searchIndex)
+    const result = await getResultByPath(library, decodeURIComponent(params.provider), decodeURIComponent(params.id))
+    return { props: { result } }
+  }
+</script>
 <script lang="ts">
   import Header from '$lib/header/Header.svelte'
   import ResultTile from '$lib/ResultTile.svelte'
 
   export let result
 </script>
+<svelte:head>
+	<title>{result.provider}’s “{result.title || result.keywords.join(' ')}”</title>
+</svelte:head>
+
 
 <Header />
 
-<ResultTile data={result} />
+<div>
+  <ResultTile data={result} expand />
+</div>
+
+<style>
+  div {
+    max-width: 900px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 50vh;
+  }
+</style>
--- src/routes/sign/[provider]/[id].ts
+++
@@ -1,8 +0,0 @@
-import * as searchIndex from '$lib/search/search-index'
-import siteConfig from '$lib/site-config.json'
-
-export async function get ({ params }) {
-  const library = await searchIndex.open(siteConfig.searchIndex)
-  const result = await searchIndex.getResultByPath(library, params.provider, params.id)
-  return { body: { result } }
-}
