3c41baeb1bfd65249ae8d45e6514d10e615e2429
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-04-24 01:32:18 +1000

    functions/hash tries to be more isomorphic

--- src/lib/search/hash.ts
+++ src/lib/search/hash.ts
@@ -1,9 +1,20 @@
+async function getCrypto (): Promise<SubtleCrypto> {
+  if ('crypto' in globalThis) {
+    return globalThis.crypto.subtle
+  } else {
+    /** @ts-ignore */
+    return (await import('crypto')).webcrypto.subtle
+  }
+}
+
 /**
  * sha256 a string or byte array
  */
 export async function sha256 (message: string | Uint8Array): Promise<Uint8Array> {
   const msgUint8 = typeof message === 'string' ? new TextEncoder().encode(message) : message
-  const hashBuffer = await window.crypto.subtle.digest('SHA-256', msgUint8)
+  /** @ts-ignore */
+  const subtle = await getCrypto()
+  const hashBuffer = await subtle.digest('SHA-256', msgUint8)
   return new Uint8Array(hashBuffer)
 }
 
