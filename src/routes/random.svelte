055998c400ed6b6091278ad1e2bb3283358160e2
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-04-24 01:33:16 +1000

    much simpler /random implementation, with more caching

--- src/routes/random.svelte
+++ src/routes/random.svelte
@@ -1,19 +1,8 @@
 <script lang="ts" context="module">
-  export const prerender = true
-</script>
-
-<svelte:head>
-  <title>Random Sign</title>
-</svelte:head>
-
-<script lang="ts">
-  import siteConfig from '$lib/site-config.json'
-  import Header from '$lib/header/Header.svelte'
-  import ResultTile from '$lib/ResultTile.svelte'
-  import Spinner from '$lib/Spinner.svelte'
-  import { onMount } from 'svelte'
-  import { open, freshen, getResult } from '$lib/search/search-index'
-  import { fade } from 'svelte/transition'
+  import type { SearchDataItem } from '$lib/search/search-index'
+  import { getSearchLibrary } from '$lib/search/search'
+  import { getResult } from '$lib/search/search-index'
+  import { browser } from '$app/env'
 
   const blockedTags = [
     'signpedia', // block signpedia-like entries, not established signs
@@ -22,27 +11,69 @@
     'semantic.sexuality' // block formal register sexual body parts type of words
   ]
 
-  let result
-  let library
-
-  async function reroll () {
-    // clear current result
-    result = undefined
-    library = await freshen(library)
-    const random = Object.create(library.index[Math.round(Math.random() * (library.index.length - 1))])
-    const tagMatch = blockedTags.some(x => random.tags.includes(x))
-    if (!tagMatch) {
-      // load the result and set it up as the answer
-      result = await getResult(library, random)
+  import type { Load } from './random'
+  export const load: Load = async function load ({ stuff }) {
+    if (browser) {
+      const library = await getSearchLibrary()
+      for (let i = 0; i < 100; i++) {
+        const random = library.index[Math.round(Math.random() * (library.index.length - 1))]
+        const tagMatch = blockedTags.some(x => random.tags.includes(x))
+        if (!tagMatch) {
+          // load the result and set it up as the answer
+          return {
+            props: {
+              loading: false,
+              result: await getResult(library, random),
+              error: undefined
+            }
+          }
+        }
+      }
+      return { props: { loading: false, result: undefined, error: 'failed to find a suitable result' } }
     } else {
-      reroll()
+      return { props: { loading: true, result: undefined, error: undefined } }
     }
   }
+</script>
+<svelte:head>
+  <title>Random Sign</title>
+</svelte:head>
+<script lang="ts">
+  // import siteConfig from '$lib/site-config.json'
+  import Header from '$lib/header/Header.svelte'
+  import ResultTile from '$lib/ResultTile.svelte'
+  import Spinner from '$lib/Spinner.svelte'
+  // import { onMount } from 'svelte'
+  // import { open, freshen, getResult } from '$lib/search/search-index'
+  import { fade } from 'svelte/transition'
+import { invalidate } from '$app/navigation';
 
-  onMount(async () => {
-    library = await open(siteConfig.searchIndex)
-    reroll()
-  })
+  export let result: SearchDataItem | undefined
+  export let loading: boolean
+  export let error: string | undefined
+
+  // async function reroll () {
+  //   // clear current result
+  //   result = undefined
+  //   library = await freshen(library)
+  //   const random = Object.create(library.index[Math.round(Math.random() * (library.index.length - 1))])
+  //   const tagMatch = blockedTags.some(x => random.tags.includes(x))
+  //   if (!tagMatch) {
+  //     // load the result and set it up as the answer
+  //     result = await getResult(library, random)
+  //   } else {
+  //     reroll()
+  //   }
+  // }
+
+  function reroll () {
+    invalidate('/random')
+  }
+
+  // onMount(async () => {
+  //   library = await open(siteConfig.searchIndex)
+  //   reroll()
+  // })
 </script>
 
 <Header/>
@@ -50,16 +81,18 @@
 <main>
   <h1>Random Sign Generator</h1>
 
-  <h2><button disabled={!library} on:click={reroll}>🎲 Reroll</button></h2>
+  <h2><button disabled={loading} on:click={reroll}>🎲 Reroll</button></h2>
 
   {#if result}
     <div class="result-box" transition:fade={{ duration: 100 }}>
       <ResultTile bind:data={result} expand/>
     </div>
-  {:else}
+  {:else if loading}
     <div class=spacer>
       <Spinner/>
     </div>
+  {:else if error}
+    <h1>{error}</h1>
   {/if}
 
   <div style="height: 20em"></div>
