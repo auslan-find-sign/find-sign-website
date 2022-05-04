4d61f3470b0243bc86d68ab73698d34fc35a8673
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-05-04 19:45:37 +1000

    $lib/functions/delay.lagSwitch()

--- src/lib/functions/delay.ts
+++ src/lib/functions/delay.ts
@@ -1,9 +1,44 @@
 /**
  * Delay until promise resolves
  */
-export async function delay (duration: number) {
-  await new Promise((resolve, reject) => { setTimeout(() => resolve(undefined), duration) })
-  return
+export async function delay<Type> (duration: number, value: Type = undefined): Promise<Type> {
+  await new Promise((resolve, _) => { setTimeout(() => resolve(undefined), duration) })
+  return value
+}
+
+/**
+ * lagSwitch will run a function, and wait for it's promise to resolve. If the promise takes
+ * longer than maxDelay to resolve, it returns defaultValue instead, and calls laterDo with
+ * the final value when it arrives. We can optimistically try to load slow resources, and
+ * return quickly if they're taking too long, or return accurately if they're cached or fast
+ *
+ * If a laggy promise rejects, laterDo is called with undefined as value, and reason as second argument
+ */
+export async function lagSwitch<Type> (maxDelay: number, promise: Promise<Type>, defaultValue: Type, laterDo: (Type, error?: string) => any): Promise<Type> {
+  return await new Promise((resolve, reject) => {
+    let timeout
+    const giveup = () => {
+      resolve(defaultValue)
+      timeout = undefined
+    }
+    timeout = setTimeout(giveup, maxDelay)
+    promise.then(value => {
+      if (timeout) {
+        clearTimeout(timeout)
+        resolve(value)
+      } else {
+        laterDo(value)
+      }
+    })
+    promise.catch(reason => {
+      if (timeout) {
+        clearTimeout(timeout)
+        reject(timeout)
+      } else {
+        laterDo(undefined, reason)
+      }
+    })
+  })
 }
 
 export default delay
--- tests/functions.delay.test.ts
+++ tests/functions.delay.test.ts
@@ -1,13 +1,31 @@
 import { expect, describe, it } from 'vitest'
 import { delay, lagSwitch } from '../src/lib/functions/delay.js'
 
-describe('/src/lib/functions/delay.ts delay()', () => {
-  it('seems accurate', async () => {
+describe('/src/lib/functions/delay.ts', () => {
+  it('delay()', async () => {
     const start = Date.now()
-    await delay(20)
+    await delay(25)
     const end = Date.now()
 
-    expect(end - start).to.be.above(18)
-    expect(end - start).to.be.below(22)
+    expect(end - start).to.be.within(20, 30)
+
+    expect(await delay(1, 'foo')).to.equal('foo')
+    expect(await delay(1)).to.equal(undefined)
+  })
+
+  it('lagSwitch()', async () => {
+    let finalValue = undefined
+    const ret = await lagSwitch(10, delay(15, 'final'), 'default', value => { finalValue = value })
+    expect(ret).to.equal('default')
+    expect(finalValue).to.equal(undefined)
+    await delay(10)
+    expect(finalValue).to.equal('final')
+
+    let final2 = undefined
+    const ret2 = await lagSwitch(10, delay(5, 'final'), 'default', value => { final2 = value })
+    expect(ret2).to.equal('final')
+    expect(final2).to.equal(undefined)
+    await delay(10)
+    expect(final2).to.equal(undefined)
   })
 })
