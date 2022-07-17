d7db421e4030e1d1202c7022493a3cc769464950
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-07-17 20:50:02 +1000

    build global vector cache to reduce initial pageload by 2mb

--- src/routes/admin/indexes/[provider]/build.ts
+++ src/routes/admin/indexes/[provider]/build.ts
@@ -1,8 +1,9 @@
-import { bulkWrite } from '$lib/data-io/data-io'
+import { bulkWrite, readFile, writeFile } from '$lib/data-io/data-io'
 import { userHasPower } from '$lib/models/user'
 import { readEncodedSearchData, writeIndex } from '$lib/orthagonal/write'
 import { createProgressLog, nextUpdate } from '$lib/progress/progress-log'
 import { lookup } from '$lib/search/loaded-precomputed-vectors'
+import { build, parse } from '$lib/search/precomputed-vectors'
 import { LoginRedirect } from '../../login'
 
 /** long polling endpoint for progress and log streaming from the post endpoints long running output */
@@ -28,6 +29,22 @@ export async function post ({ locals, params }) {
     log(`Search index files constructed, writing...`)
     progress(0.96)
     await bulkWrite(`index/${params.provider}`, files)
+    progress(0.97)
+    log(`Rebuilding global word vector cache: Loading vector files...`)
+    const vectorFiles = await Promise.all(import.meta.env.VITE_SEARCH_INDEXES.split(',').map(async provider =>
+      parse(await readFile(`index/${provider}/vectors.lps`))
+    ))
+    progress(0.98)
+    log(`Building global word vector cache...`)
+    const globalVectors = {}
+    for (const vectors of vectorFiles) {
+      for (const { word, getVector } of vectors) {
+        if (!globalVectors[word]) globalVectors[word] = getVector()
+      }
+    }
+    progress(0.99)
+    log(`Writing global word vector cache...`)
+    await writeFile('index/global-vectors.lps', build(globalVectors))
     progress(1.00)
     log('Build complete')
   })
--- src/routes/automation/build-index/[provider=provider].ts
+++ src/routes/automation/build-index/[provider=provider].ts
@@ -1,7 +1,8 @@
 import { decodeFilename, fn } from '$lib/models/filename-codec'
 import { readEncodedSearchData, writeIndex } from '$lib/orthagonal/write'
 import { lookup } from '$lib/search/loaded-precomputed-vectors'
-import { bulkWrite } from '$lib/data-io/data-io'
+import { bulkWrite, readFile, writeFile } from '$lib/data-io/data-io'
+import { build, parse } from '$lib/search/precomputed-vectors'
 
 /** endpoint for cron-like tasks to automatically rebuild orthagonal indexes after encoder has done it's work */
 export async function get ({ url, params }) {
@@ -30,6 +31,21 @@ export async function get ({ url, params }) {
         })
         controller.enqueue('Search index files constructed, writing...\n')
         await bulkWrite(fn`index/${provider}`, files)
+
+        controller.enqueue(`Rebuilding global word vector cache: Loading vector files...\n`)
+        const vectorFiles = await Promise.all(import.meta.env.VITE_SEARCH_INDEXES.split(',').map(async provider =>
+          parse(await readFile(`index/${provider}/vectors.lps`))
+        ))
+        controller.enqueue(`Building global word vector cache...\n`)
+        const globalVectors = {}
+        for (const vectors of vectorFiles) {
+          for (const { word, getVector } of vectors) {
+            if (!globalVectors[word]) globalVectors[word] = getVector()
+          }
+        }
+        controller.enqueue(`Writing global word vector cache...\n`)
+        await writeFile('index/global-vectors.lps', build(globalVectors))
+
         controller.enqueue('Done\n')
         controller.close()
       }
