ebe89bd1d753092b026af26dbdc2f98ca8721f0b
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-05 11:07:44 +1000

    improving interfaces and comments

--- src/lib/search/search.ts
+++ src/lib/search/search.ts
@@ -1,5 +1,3 @@
-// import { getResult, type Library, type SearchDataItem } from './search-index'
-// import { open, freshen, getResultByNumericPath } from '$lib/search/search-index'
 import { loadIndex, OutdatedError, type LoadedOrthagonalIndex } from '$lib/orthagonal/read'
 import type { EncodedSearchDataEntry } from '$lib/orthagonal/types'
 import { lookup } from '$lib/search/loaded-precomputed-vectors'
@@ -11,25 +9,32 @@ import openGlobalVectors from './global-vectors'
 import type { WordVector } from './precomputed-vectors'
 
 export const availableIndexes = import.meta.env.VITE_SEARCH_INDEXES.split(',')
+// an object of index names to url's where that index is published
 export const indexURLs = Object.fromEntries(availableIndexes.map(name => {
   return [name, `${import.meta.env.VITE_SEARCH_INDEX_PATH}/${encodeFilename(name)}`]
 }))
+// an object of index names, keyed by the url to that index, for reverse lookup
 export const indexByURL = Object.fromEntries(Object.entries(indexURLs).map(x => x.reverse()))
 
+// return type of search()
 export type SearchResponse = {
   results: SearchResult[],
   totalResults: number
 }
 
+// key-value object of all the loaded search indexes as a library collection
 export type SearchLibrary = {
   [name: string]: LoadedOrthagonalIndex
 }
 
+// this is the type search() returns an array of in it's results property, and what
+// the Result.svelte component expects to present a search result
 export interface SearchResult extends EncodedSearchDataEntry {
   index: string, // string search index name, e.g. "auslan-signbank"
 }
 
-/** limit libraries maybe an array of provider ids */
+// getSearchLibrary loads a collection of search libraries, possibly limited to a specific set
+// of indexes (e.g. auslan-signbank) and a specific set of columns (words, tags, link, id, author, etc)
 export async function getSearchLibrary (limitLibraries: string[] | false = false, columns = ['words', 'tags'], forceReload = false, globalVectors?: { [word: string]: WordVector }): Promise<SearchLibrary> {
   if (limitLibraries === false) limitLibraries = Object.keys(indexURLs)
 
@@ -42,20 +47,27 @@ export async function getSearchLibrary (limitLibraries: string[] | false = false
   return collection
 }
 
+// this is the main guts of the search engine, queries come in, and results come out
 export async function search (query: string, start: number, length: number, forceReload = false): Promise<SearchResponse> {
-  // load the globalVector cache
+  // load the globalVector cache, this file contains a word-vector pairing for every word that
+  // exists in the search index (in the result titles/words lists)
   let globalVectors: { [word: string]: Float32Array }
   if (/*!globalVectors &&*/ checkQueryContainsWords(query)) {
     // !!! should this have a cache expiry mechanism?
     globalVectors = await openGlobalVectors(`${import.meta.env.VITE_SEARCH_INDEX_PATH}/global-vectors.lps`)
   }
 
+  // word lookup function which checks the local vectors cache before bothering to load rare words
+  // from the word vector library
   const lookupLocal = async function (word) {
     if (globalVectors && globalVectors[word]) return globalVectors[word]
     return lookup(word)
   }
 
-
+  // parse and compile the user's search query string in to a list of which data needs to be loaded
+  // and a rank function. requirements lists the columns of the dataset that are needed to run rank
+  // and rank accepts a search library entry and returns a distance number, with 0 being
+  // a perfect match
   const compiledQuery = await compileQuery(query, async (word) => {
     const normalized = normalizeWord(word)
     const normalizedResult = await lookupLocal(normalized)
@@ -67,14 +79,19 @@ export async function search (query: string, start: number, length: number, forc
     }
   })
 
+  // load the search libraries, only bothering to fetch the columns necessary to run the rank function
   const { requirements: columns, rank: rankFn } = compiledQuery
   const library = await getSearchLibrary(false, columns, forceReload, globalVectors)
 
+  // sort the libraries in to one set of search results
   const rankedIndex = rank(library, rankFn)
 
+  // slice out the results that are wanted (which page, basically)
   const resultEntries = rankedIndex.entries.slice(start, start + length)
   let results: SearchResult[]
   try {
+    // load the full search result json for each result, so we have everything needed to present
+    // to user
     results = await Promise.all(resultEntries.map(async entry => {
       const loadedResult: SearchResult = {
         ...await entry.load(),
@@ -83,6 +100,11 @@ export async function search (query: string, start: number, length: number, forc
       return loadedResult
     }))
   } catch (error) {
+    // if an OutdatedError throws, the currently loaded in memory search libraries are not up
+    // to date with the current version on the server, so the whole thing is re-run with
+    // forceReload set to true, causing the browser to fetch with cache invalidating headers
+    // In this spot, an OutdatedError is likely to throw because a search library was updated
+    // after the user loaded the library, but before the user loaded the search result json
     if (error instanceof OutdatedError) {
       if (forceReload === false) {
         return await search(query, start, length, true)
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,12 @@
 	import DiscoveryFeed from '$lib/DiscoveryFeed.svelte'
 	import { onMount } from 'svelte'
 	import { prefetchRoutes } from '$app/navigation'
+	import openGraphImage from '$lib/assets/open-graph-image@3x.png'
+	import { page } from '$app/stores'
 
 	export let feed
+
+	const openGraphImageFullURL = (new URL(openGraphImage, $page.url)).toString()
 
 	onMount(() => {
 		// preload the search results interface
@@ -14,7 +18,7 @@
 
 <svelte:head>
 	<title>Find Sign</title>
-	<meta property=og:image content=https://find.auslan.fyi/style/assets/open-graph-image@3x.png>
+	<meta property=og:image content={openGraphImageFullURL}>
   <meta property=og:description content="Find Sign is an Auslan Search Engine. It helps find Auslan resources from around the internet in one place.">
   <meta property=og:title content="Find Sign - Auslan Search Engine">
 </svelte:head>
