664ab603f12cde9163b6951e9b3fdb37a1bf2c9b
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-15 13:50:09 +1000

    even nicer styling

--- src/routes/sign/[provider=provider]/index.svelte
+++ src/routes/sign/[provider=provider]/index.svelte
@@ -28,7 +28,7 @@
   <h1>List of “{provider}” entries</h1>
   <ul>
     {#each results as { id, title }}
-      <li><a href={`/sign/${provider}/${id}`}>{title} <span>({id})</span></a></li>
+      <li><a href={`/sign/${provider}/${id}`}>{title}</a> <span>({id})</span></li>
     {/each}
   </ul>
 </MainBlock>
@@ -38,7 +38,7 @@
     opacity: 0;
     font-size: 0.9em;
   }
-  ul li a:hover span {
+  ul li:hover a span {
     opacity: 0.5;
   }
 </style>
