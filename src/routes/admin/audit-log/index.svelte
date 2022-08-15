20e9304dfe4273f24abbb4af3a33d62d6fe5b56a
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-15 13:44:28 +1000

    better audit log formatting

--- src/routes/admin/audit-log/index.svelte
+++ src/routes/admin/audit-log/index.svelte
@@ -14,14 +14,26 @@
 
 {#each pageEntries as entry}
   <MainBlock>
-    <p><a href="/admin/users/{encodeURIComponent(entry.actor)}">{entry.actor}</a> performed <code>{entry.actionType}</code></p>
+    <h1><a href="/admin/users/{encodeURIComponent(entry.actor)}">{entry.actor}</a> performed <code>{entry.actionType}</code></h1>
     <p><code>{entry.message}</code></p>
     {#if entry.extra}
-      {#if entry.extra.publicURL}Public URL: <a href={entry.extra.publicURL}>{entry.extra.publicURL}</a>{/if}
-      {#if entry.extra.adminURL}Admin URL: <a href={entry.extra.adminURL}>{entry.extra.adminURL}</a>{/if}
-      {#if entry.extra.index}Index: <a href={`/sign/${encodeFilename(entry.extra.index)}`}>{entry.extra.index}</a>{/if}
-      {#if entry.extra.index && entry.extra.entryID}Entry ID: <a href={`/sign/${encodeFilename(entry.extra.index)}/${encodeFilename(entry.extra.entryID)}`}>{entry.extra.entryID}</a>{/if}
-      {#if entry.extra.object}<pre><code>{JSON.stringify(entry.extra.object, null, 2)}</code></pre>{/if}
+      <div>
+        {#if entry.extra.publicURL}
+          <div>Public URL: <a href={entry.extra.publicURL}>{entry.extra.publicURL}</a></div>
+        {/if}
+        {#if entry.extra.adminURL}
+          <div>Admin URL: <a href={entry.extra.adminURL}>{entry.extra.adminURL}</a></div>
+        {/if}
+        {#if entry.extra.index}
+          <div>Index: <a href={`/sign/${encodeFilename(entry.extra.index)}`}>{entry.extra.index}</a></div>
+        {/if}
+        {#if entry.extra.index && entry.extra.entryID}
+          <div>Entry ID: <a href={`/sign/${encodeFilename(entry.extra.index)}/${encodeFilename(entry.extra.entryID)}`}>{entry.extra.entryID}</a></div>
+        {/if}
+        {#if entry.extra.object}
+          <pre><code>{JSON.stringify(entry.extra.object, null, 2)}</code></pre>
+        {/if}
+      </div>
     {/if}
     <p><time>{humane(entry.time)} {entry.time.toLocaleTimeString()}</time></p>
   </MainBlock>
