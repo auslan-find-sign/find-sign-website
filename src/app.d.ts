d88d50b63c8609fd6597c533ed8186c02c6c5ae7
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-21 22:51:53 +1000

    fix app.d.ts ambient file import issue

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,18 +1,15 @@
 /// <reference types="@sveltejs/kit" />
-import type { Username } from '$lib/models/user'
 
 // See https://kit.svelte.dev/docs/types#the-app-namespace
 // for information about these interfaces
 declare namespace App {
 	interface Locals {
-		username?: Username;
+		username?: import("$lib/models/user").Username;
 	}
 
 	// interface Platform {}
 
-	interface Session {
-		username?: Username;
-	}
+	// interface PrivateEnv {}
 
-	// interface Stuff {}
+	// interface PublicEnv {}
 }
