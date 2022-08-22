662e204ef2c22823ab6bb21b77b2d9f0c895d161
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-22 13:22:29 +1000

    switch build log to EventStream

--- src/routes/admin/indexes/[provider]/build/+server.ts
+++ src/routes/admin/indexes/[provider]/build/+server.ts
@@ -1,24 +1,20 @@
-import { json } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit'
 import { writeAuditLog, type AuditExtraData } from '$lib/models/audit-log'
 import { fn } from '$lib/models/filename-codec'
 import { userHasPower } from '$lib/models/user'
 import buildSearchIndex from '$lib/orthagonal/build'
-import { createProgressLog, nextUpdate } from '$lib/progress/progress-log'
+import { createProgressLog, nextUpdateResponse } from '$lib/progress/progress-log'
 import { LoginRedirect } from '../../../login/passkey/+server'
+import type { RequestHandler } from './$types'
 
 /** long polling endpoint for progress and log streaming from the post endpoints long running output */
-export async function GET ({ url }) {
-  const update = await nextUpdate(url.searchParams.get('id'), parseInt(url.searchParams.get('index')))
-  throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292701)");
-  // Suggestion (check for correctness before using):
-  // return json(update);
-  return { body: update }
+export const GET: RequestHandler = async (event) => {
+  return await nextUpdateResponse(event)
 }
 
 /** endpoint for manually rebuilding search index, e.g. after authoring an index override */
-export async function POST ({ locals, params, url }) {
-  if (!locals.username) throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292701)");
-  return LoginRedirect
+export const POST: RequestHandler = async ({ locals, params, url }) => {
+  if (!locals.username) return LoginRedirect
   if (!await userHasPower(locals.username, 'edit-index')) throw new Error('You don’t have the right to edit search index')
   const fast = !!url.searchParams.has('fast')
 
