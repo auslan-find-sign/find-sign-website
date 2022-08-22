8822a1cc1738aa4cb7cbf60239ff3063ebff926d
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-22 13:22:29 +1000

    switch build log to EventStream

--- src/lib/progress/progress-consumer.ts
+++ src/lib/progress/progress-consumer.ts
@@ -1,19 +1,55 @@
 import delay from '$lib/functions/delay'
 
+export type ProgressReport = {
+  progress?: number,
+  log?: any[],
+  finished?: boolean,
+  timestamp?: number,
+}
+
 /** async iterator outputs progress stream over long polling endpoint from ./progress-log */
-export default async function * progressConsumer (endpoint: string, id: string) {
-  let index = 0
-  let finished = false
-  while (!finished) {
-    const path = new URL(endpoint, location.href)
-    path.searchParams.set('id', id)
-    path.searchParams.set('index', `${index}`)
-    const response = await fetch(path.toString())
-    const json = await response.json()
-    if ('index' in json) index = json.index
-    if ('finished' in json) finished = json.finished
-    if ('progress' in json) yield { progress: json.progress }
-    if ('logs' in json && json.logs.length > 0) yield * json.logs
-    await delay(15)
+export default async function * progressConsumer (endpoint: string, id: string): AsyncIterable<ProgressReport> {
+  // old long polling implementation
+  // let index = 0
+  // let finished = false
+  // while (!finished) {
+  //   const path = new URL(endpoint, location.href)
+  //   path.searchParams.set('id', id)
+  //   path.searchParams.set('index', `${index}`)
+  //   const response = await fetch(path.toString())
+  //   const json = await response.json()
+  //   if ('index' in json) index = json.index
+  //   if ('finished' in json) finished = json.finished
+  //   if ('progress' in json) yield { progress: json.progress }
+  //   if ('logs' in json && json.logs.length > 0) yield * json.logs
+  //   await delay(15)
+  // }
+
+  const buffer = new TransformStream()
+  const writer = buffer.writable.getWriter()
+  const reader = buffer.readable.getReader()
+
+  const path = new URL(endpoint, location.href)
+  path.searchParams.set('id', id)
+  const source = new EventSource(path)
+  let timestamp = Date.now()
+  source.addEventListener('log', event => writer.write({ log: JSON.parse(event.data) }))
+  source.addEventListener('progress', event => writer.write({ progress: JSON.parse(event.data) }))
+  source.addEventListener('timestamp', event => timestamp = JSON.parse(event.data))
+  source.addEventListener('finished', event => {
+    writer.write({ finished: true })
+    writer.close()
+    source.close()
+  })
+
+  while (true) {
+    const result = await reader.read()
+    if (result.value) {
+      yield result.value
+    }
+    if (result.done) {
+      reader.cancel()
+      return
+    }
   }
 }
--- src/lib/progress/progress-log.ts
+++ src/lib/progress/progress-log.ts
@@ -3,6 +3,7 @@
 // to respond with a progress ID, and then subsequent requests can ask for the next update until
 // the process is complete.
 // notably used in /admin/indexes/.../build
+import { json, type RequestEvent } from '@sveltejs/kit'
 import { nanoid } from 'nanoid'
 
 type ProgressReport = {
@@ -96,5 +97,64 @@ export async function nextUpdate (id: string, index: number): Promise<ProgressRe
     return sliceUpdates(id, index)
   } else {
     return { id, progress: 1, logs: [], finished: true }
+  }
+}
+
+// supports both Event Stream and long polling response types
+export async function nextUpdateResponse (event: RequestEvent): Promise<Response> {
+  const id = event.url.searchParams.get('id')
+
+  const accept = event.request.headers.get('Accept') || ''
+  if (accept === 'text/event-stream') {
+    const lastEventID = event.request.headers.get('Last-Event-ID')
+    let lastIndex = lastEventID ? parseInt(lastEventID) : -1
+    return new Response(new ReadableStream({
+      start (controller) {
+        if (!inProgress[id]) {
+          // send back an immediate finished response
+          controller.enqueue('event: finished\n data: true\n\n')
+          controller.close()
+        }
+      },
+      async pull (controller) {
+        const updates = inProgress[id]
+        if (updates) {
+          const catchup = updates.slice(lastIndex + 1)
+          for (const update of catchup) {
+            lastIndex += 1
+
+            if (update.progress) {
+              controller.enqueue(`event:progress\ndata:${JSON.stringify(Math.round(update.progress * 10000) / 10000)}\nid:${JSON.stringify(lastIndex)}\n\n`)
+            }
+            if (update.log) {
+              controller.enqueue(`event:log\ndata:${JSON.stringify(update.log)}\nid:${JSON.stringify(lastIndex)}\n\n`)
+            }
+            if (update.finished) {
+              controller.enqueue(`event:finished\ndata:${JSON.stringify(update.finished)}\nid:${JSON.stringify(lastIndex)}\n\n`)
+              controller.close()
+              delete inProgress[id]
+              delete subscribers[id]
+              return
+            }
+          }
+
+          // wait for next update
+          await new Promise(resolve => {
+            if (subscribers[id]) {
+              subscribers[id].push(() => resolve(undefined))
+            } else {
+              resolve(undefined)
+            }
+          })
+        } else {
+          // send back an immediate finished response
+          controller.enqueue('event:finished\ndata: true\n\n')
+          controller.close()
+        }
+      }
+    }), { headers: { 'Content-Type': 'text/event-stream' } })
+  } else {
+    const index = event.url.searchParams.get('index')
+    return json(await nextUpdate(id, parseInt(index)))
   }
 }
