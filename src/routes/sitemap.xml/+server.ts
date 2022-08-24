0c524f95f2c7679dfb1549ee574829144a679578
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-25 01:18:46 +1000

    sitemap includes <lastmod> where possible

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -2,7 +2,6 @@ import { fn } from '$lib/models/filename-codec'
 import { getSearchLibrary, availableIndexes } from '$lib/search/search'
 
 const staticPages = [
-  '/',
   '/about',
   '/technology',
   '/random',
@@ -11,12 +10,20 @@ const staticPages = [
   '/feeds/discovery.rss',
 ]
 
+function lastmod (timestamp) {
+  const date = new Date(timestamp)
+  const prefix = (str) => `00${str}`.slice(-2)
+  return `<lastmod>${date.getUTCFullYear()}-${prefix(date.getUTCMonth() + 1)}-${prefix(date.getUTCDate())}</lastmod>`
+}
+
 export async function GET ({ url }) {
   const indexes = [...availableIndexes]
   return new Response(new ReadableStream({
     start (controller) {
       controller.enqueue('<?xml version="1.0" encoding="UTF-8" ?>\n')
       controller.enqueue('<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">\n')
+
+      controller.enqueue(`<url><loc>${new URL('/', url)}</loc><changefreq>daily</changefreq></url>`)
 
       for (const path of staticPages) {
         controller.enqueue(`<url><loc>${new URL(path, url)}</loc></url>\n`)
@@ -27,13 +34,19 @@ export async function GET ({ url }) {
     async pull (controller) {
       if (indexes.length > 0) {
         const index = indexes.shift()
-        controller.enqueue(`<url><loc>${new URL(fn`/sign/${index}`, url)}</loc></url>\n`)
 
-        const libraries = await getSearchLibrary([index], ['id'])
+        const libraries = await getSearchLibrary([index], ['id', 'timestamp'])
         const lib = libraries[index]
 
-        for (const { id } of lib.entries) {
-          controller.enqueue(`<url><loc>${new URL(fn`/sign/${index}/${id}`, url)}</loc></url>\n`)
+        controller.enqueue(`<url><loc>${new URL(fn`/sign/${index}`, url)}</loc>${lastmod(lib.meta.buildTimestamp)}</url>\n`)
+
+        for (const { id, timestamp } of lib.entries) {
+          controller.enqueue('<url>')
+          controller.enqueue(`<loc>${new URL(fn`/sign/${index}/${id}`, url)}</loc>`)
+          if (timestamp) {
+            controller.enqueue(lastmod(timestamp))
+          }
+          controller.enqueue('</url>\n')
         }
       } else {
         controller.enqueue('</urlset>\n')
