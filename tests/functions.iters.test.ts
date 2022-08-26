0215bf35315880a699219cfae19f948220785d11
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-26 12:56:33 +1000

    iters.chunk uses slice instead of collecting iterations for faster chunking of arrays

--- tests/functions.iters.test.ts
+++ tests/functions.iters.test.ts
@@ -1,5 +1,5 @@
 import { expect, describe, it } from 'vitest'
-import { count, times } from '../src/lib/functions/iters.js'
+import { count, times, chunk } from '../src/lib/functions/iters.js'
 
 describe('/src/lib/functions/iters.ts', () => {
   it('count() seems accurate', () => {
@@ -12,5 +12,13 @@ describe('/src/lib/functions/iters.ts', () => {
     expect([...times(0, x => x * 2)]).to.deep.equal([])
     expect([...times(3, x => x * 2)]).to.deep.equal([0, 2, 4])
     expect([...times(3, x => `${x}`)]).to.deep.equal(['0', '1', '2'])
+  })
+
+  it('chunk() seems to work', () => {
+    expect(chunk('', 2)).to.deep.equal([''])
+    expect(chunk('123456789', 2)).to.deep.equal(['12', '34', '56', '78', '9'])
+    expect(chunk('123456789', 3)).to.deep.equal(['123', '456', '789'])
+    expect(chunk('123456789', 2, 1)).to.deep.equal(['12'])
+    expect(chunk('123456789', 2, 2)).to.deep.equal(['12', '34'])
   })
 })
