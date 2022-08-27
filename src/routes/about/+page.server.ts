61ff5236964e8bbd3d6702f212187322dc2a14e0
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-28 02:29:38 +1000

    about page is now immidiateResponseCached

--- src/routes/about/+page.server.ts
+++ src/routes/about/+page.server.ts
@@ -1,42 +1,33 @@
 import type { PageServerLoad } from './$types'
-import cache from '$lib/functions/cache'
+import { immediateResponseCache } from '$lib/functions/cache'
 import { getSearchLibrary } from '$lib/search/search'
 
-const maxAge = 60 * 60 // 1 hour
-const cached = cache(maxAge)
+export const load: PageServerLoad = immediateResponseCache(async () => {
+  const tagAccumulator = {}
+  const userAccumulator = {}
 
-export const load: PageServerLoad = async ({ setHeaders }) => {
-  setHeaders({ 'Cache-Control': `max-age=${maxAge}` })
+  const libraries = await getSearchLibrary(false, ['tags', 'author'])
 
-  if (cached.get()) {
-    return cached.get()
-  } else {
-    const tagAccumulator = {}
-    const userAccumulator = {}
-
-    const libraries = await getSearchLibrary(false, ['tags', 'author'])
-
-    for (const provider in libraries) {
-      for (const entry of libraries[provider].entries) {
-        for (const tagName of entry.tags) {
-          if (!tagAccumulator[tagName]) tagAccumulator[tagName] = 0
-          tagAccumulator[tagName] += 1
-        }
-        if (entry.author && entry.author.id) {
-          if (!userAccumulator[entry.author.id]) userAccumulator[entry.author.id] = 0
-          userAccumulator[entry.author.id] += 1
-        }
+  for (const provider in libraries) {
+    for (const entry of libraries[provider].entries) {
+      for (const tagName of entry.tags) {
+        if (!tagAccumulator[tagName]) tagAccumulator[tagName] = 0
+        tagAccumulator[tagName] += 1
+      }
+      if (entry.author && entry.author.id) {
+        if (!userAccumulator[entry.author.id]) userAccumulator[entry.author.id] = 0
+        userAccumulator[entry.author.id] += 1
       }
     }
-
-
-    const hashtags = Object.entries(tagAccumulator)
-      .map(([hashtag, count]: [string, number]) => ({ hashtag, count }))
-      .sort((left, right) => right.count - left.count)
-    const usernames = Object.entries(userAccumulator)
-      .map(([username, count]: [string, number]) => ({ username, count }))
-      .sort((left, right) => right.count - left.count)
-
-    return cached.set({ hashtags, usernames })
   }
-}
+
+
+  const hashtags = Object.entries(tagAccumulator)
+    .map(([hashtag, count]: [string, number]) => ({ hashtag, count }))
+    .sort((left, right) => right.count - left.count)
+  const usernames = Object.entries(userAccumulator)
+    .map(([username, count]: [string, number]) => ({ username, count }))
+    .sort((left, right) => right.count - left.count)
+
+  return { hashtags, usernames }
+})
