00c8e825faa49225490240d653f1347037071da5
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-28 02:34:41 +1000

    remove random from sitemap, hint more changefreq items

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,7 +4,6 @@ import { getSearchLibrary, availableIndexes } from '$lib/search/search'
 const staticPages = [
   '/about',
   '/technology',
-  '/random',
   '/feeds/discovery.atom',
   '/feeds/discovery.json',
   '/feeds/discovery.rss',
@@ -26,10 +25,10 @@ export async function GET ({ url }) {
       controller.enqueue(`<url><loc>${new URL('/', url)}</loc><changefreq>daily</changefreq></url>`)
 
       for (const path of staticPages) {
-        controller.enqueue(`<url><loc>${new URL(path, url)}</loc></url>\n`)
+        controller.enqueue(`<url><loc>${new URL(path, url)}</loc><changefreq>yearly</changefreq></url>\n`)
       }
 
-      controller.enqueue(`<url><loc>${new URL(fn`/sign/`, url)}</loc></url>\n`)
+      controller.enqueue(`<url><loc>${new URL(fn`/sign/`, url)}</loc><changefreq>yearly</changefreq></url>\n`)
     },
     async pull (controller) {
       if (indexes.length > 0) {
