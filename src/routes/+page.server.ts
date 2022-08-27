f6d2e48de3ca83368e392ba3ce9c1c7a4b3432b6
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-28 02:33:33 +1000

    switch homepage to immediateResponseCache so it feels responsive always

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,49 +1,40 @@
 import type { PageServerLoad } from './$types'
 import { getUpdatesFeed } from './feeds/_read-discovery-feed'
-import cache from '$lib/functions/cache'
+import { immediateResponseCache } from '$lib/functions/cache'
 import readFeed from '$lib/data-io/rss'
 
 export const prerender = false
-const maxAge = 60 * 5
 
 const feedEntries = 20
 
-// stores old feed output for homepage feed
-const cachedFeed = cache(maxAge)
-
-export const load: PageServerLoad = async ({ url, setHeaders }) => {
-  let body = cachedFeed.get()
-  if (!body) {
-    const [searchFeed, rssFeed] = await Promise.all([
-      getUpdatesFeed({ url, page: 0, length: feedEntries, extended: true }),
-      (async function () {
-        try {
-          return await readFeed(import.meta.env.VITE_NEWS_RSS)
-        } catch (err) {
-          console.error('Homepage blog feed failed to load', err)
-          return []
-        }
-      })()
-    ])
-    const response = [...searchFeed.items.map(x => ({
-      author: x.authors[0],
-      verb: x.__fs_verb,
-      title: x.__fs_words.join(' '),
-      date: x.date_published,
-      cacheURL: x.url,
-      remoteURL: x.external_url
-    })).reverse(), ...rssFeed.map(entry => ({
-      author: { name: 'Find Sign' },
-      verb: 'blogged',
-      title: entry.title || 'an update about the site',
-      date: (new Date(entry.pubDate)).toISOString(),
-      remoteURL: entry.link,
-    }))].sort((x, y) => Date.parse(x.date) - Date.parse(y.date)).slice(-feedEntries)
-    body = cachedFeed.set(response)
-  }
+export const load: PageServerLoad = immediateResponseCache(async ({ url, setHeaders }) => {
+  const [searchFeed, rssFeed] = await Promise.all([
+    getUpdatesFeed({ url, page: 0, length: feedEntries, extended: true }),
+    (async function () {
+      try {
+        return await readFeed(import.meta.env.VITE_NEWS_RSS)
+      } catch (err) {
+        console.error('Homepage blog feed failed to load', err)
+        return []
+      }
+    })()
+  ])
+  const feed = [...searchFeed.items.map(x => ({
+    author: x.authors[0],
+    verb: x.__fs_verb,
+    title: x.__fs_words.join(' '),
+    date: x.date_published,
+    cacheURL: x.url,
+    remoteURL: x.external_url
+  })).reverse(), ...rssFeed.map(entry => ({
+    author: { name: 'Find Sign' },
+    verb: 'blogged',
+    title: entry.title || 'an update about the site',
+    date: (new Date(entry.pubDate)).toISOString(),
+    remoteURL: entry.link,
+  }))].sort((x, y) => Date.parse(x.date) - Date.parse(y.date)).slice(-feedEntries)
 
   const automated = url.searchParams.has('automated')
 
-  setHeaders({ 'Cache-Control': `max-age=${maxAge}` })
-  return { feed: body, automated }
-}
+  return { feed, automated }
+})
