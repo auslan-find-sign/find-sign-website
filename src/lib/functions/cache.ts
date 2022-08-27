e8177d51c72b9b33bd17b8ae540baab7859a8815
Phoenix Fox <Bluebie@users.noreply.github.com>
2022-08-28 02:29:11 +1000

    immediateResponseCache returns the most recent old value immediately, and starts loading a new updated value in the background for future requests to benefit from

--- src/lib/functions/cache.ts
+++ src/lib/functions/cache.ts
@@ -13,3 +13,36 @@ export default function cache (durationSecs = 60) {
     }
   }
 }
+
+/**
+ * returns the last value immediately, then updates for the next request
+ * if called again before update returns a new value, old value keeps being returned
+ */
+export function immediateResponseCache<Ret, Args extends any[]> (loadNewValue: (...args: Args) => Promise<Ret>): (...args: Args) => Promise<Ret> {
+  let nextPromise: Promise<Ret>
+  let initialized = false
+  let updating = false
+
+  return (...args) => {
+    if (!updating) {
+      updating = true
+      const next = loadNewValue(...args)
+      if (initialized) {
+        next.then(x => {
+          updating = false
+          initialized = true
+          nextPromise = Promise.resolve(x)
+        }).catch(err => {
+          updating = false
+          initialized = true
+          nextPromise = Promise.reject(err)
+        })
+      } else {
+        nextPromise = next
+        return next
+      }
+    }
+
+    return nextPromise
+  }
+}
